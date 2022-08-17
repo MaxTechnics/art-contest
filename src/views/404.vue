d44e4a3d942d97bb7b0ba3cf1c8beb01ae45ed40
Maxim Coppieters <58440941+MaxTechnics@users.noreply.github.com>
2022-08-18 00:21:22 +0200

    Create a 404 page
    Slapped this together in like 20 seconds. I don't expect people to be that stupid, but yknow better make sure

---
+++ src/views/404.vue
@@ -0,0 +1,9 @@
+<template>
+  Either something went really wrong, or you can't type. Check your url or contact Grady's Physics Homework (MaxTechnics)
+</template>
+
+<script>
+export default {
+	name: '404'
+}
+</script>
