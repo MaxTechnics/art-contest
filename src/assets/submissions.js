45a0579276f0766c1395fcea599d61a4cc70cd7f
Maxim Coppieters <58440941+MaxTechnics@users.noreply.github.com>
2022-08-25 21:32:41 +0200

    The last submissions

--- src/assets/submissions.js
+++ src/assets/submissions.js
@@ -6,18 +6,21 @@ import moon from '@/assets/arts/moon.jpg';
 import abdullah from '@/assets/arts/abdullah.png';
 import macch from '@/assets/arts/Animus_Concept.jpg';
 import lazyjackie from '@/assets/arts/lazyjackie.png';
-import image1 from '@/assets/arts/1.jpg';
-import image2 from '@/assets/arts/2.jpg';
-import image3 from '@/assets/arts/3.jpg';
-import image4 from '@/assets/arts/4.jpg';
-import image5 from '@/assets/arts/5.jpg';
-import image6 from '@/assets/arts/6.jpg';
-import image7 from '@/assets/arts/7.jpg';
-import image8 from '@/assets/arts/8.jpg';
-import image9 from '@/assets/arts/9.jpg';
-import image10 from '@/assets/arts/10.jpg';
-import image11 from '@/assets/arts/11.jpg';
-import problems from '@/assets/arts/problemo.jpg';
+import yeet_yourself from '@/assets/arts/yeet_yourself.jpg';
+import turboaiger from '@/assets/arts/turboaiger.png';
+
+// import image1 from '@/assets/arts/1.jpg';
+// import image2 from '@/assets/arts/2.jpg';
+// import image3 from '@/assets/arts/3.jpg';
+// import image4 from '@/assets/arts/4.jpg';
+// import image5 from '@/assets/arts/5.jpg';
+// import image6 from '@/assets/arts/6.jpg';
+// import image7 from '@/assets/arts/7.jpg';
+// import image8 from '@/assets/arts/8.jpg';
+// import image9 from '@/assets/arts/9.jpg';
+// import image10 from '@/assets/arts/10.jpg';
+// import image11 from '@/assets/arts/11.jpg';
+// import problems from '@/assets/arts/problemo.jpg';
 
 
 export default [
@@ -77,167 +80,181 @@ export default [
     //     author_id: '804754691732275230',
     //     asset: lazyjackie
     // },
+    // {
+    //     title: 'Jaiden and Jacob after the Elite Four',
+    //     description: '',
+    //     author: 'yeet yourself',
+    //     author_id: '769253933167673344',
+    //     asset: yeet_yourself
+    // },
+    // {
+    //     title: 'Torchic watches sunset',
+    //     description: '',
+    //     author: 'TurboAiger 👑🎗',
+    //     author_id: '892636003892871200',
+    //     asset: turboaiger
+    // },
 
-    //// If no other submission, yeet yourself to add here!!!!
-    {
-        title: 'Mother\'s Love',
-        description: 'Every time you drink a glass of milk or eat a piece of cheese, you harm a mother. Please go vegan.',
-        author: 'Gary L. Francione',
-        author_id: 'funky',
-        asset: image6
-    },
-    {
-        title: 'Silent Killer',
-        description: 'Cows\' milk protein may be the single most significant chemical carcinogen to which humans are exposed.',
-        author: 'T. Colin Campbell',
-        author_id: 'extremely funky',
-        asset: image7
-    },
-    {
-        title: 'Problem solving around the world',
-        description: 'In a country you don\'t know and have a problem? No worries! This chart will help you out.',
-        author: 'Grady\'s Physics Homework',
-        author_id: 'the',
-        asset: problems
-    },
-    {
-        title: 'Senseless Suffering',
-        description: 'The question is not, \'Can they reason?\' nor, \'Can they talk?\' but rather, \'Can they suffer?\'',
-        author: 'Jeremy Bentham',
-        author_id: 'quick',
-        asset: image8
-    },
-    {
-        title: 'Rabbit Intelligence',
-        description: 'If a rabbit defined intelligence the way man does, then the most intelligent animal would be a rabbit, followed by the animal most willing to obey the commands of a rabbit.',
-        author: 'Robert Brault',
-        author_id: 'brown',
-        asset: image9
-    },
-    {
-        title: 'Friendly Terms',
-        description: 'Man is the only animal that can remain on friendly terms with the victims he intends to eat until he eats them.',
-        author: 'Samuel Butler',
-        author_id: 'fox',
-        asset: image10
-    },
-    {
-        title: 'Murder of Men',
-        description: 'The time will come when men such as I will look upon the murder of animals as they now look upon the murder of men.',
-        author: 'Leonardo Da Vinci',
-        author_id: 'are',
-        asset: image11
-    },
-    {
-        title: 'Highest Ethics',
-        description: 'on-violence leads to the highest ethics, which is the goal of all evolution. Until we stop harming all other living beings, we are still savages',
-        author: 'Thomas Edison',
-        author_id: 'you',
-        asset: image1
-    },
-    {
-        title: 'Pleasure, Amusement & Convenience',
-        description: 'We do not need to eat animals, wear animals, or use animals for entertainment purposes, and our only defense of these uses is our pleasure, amusement, and convenience.',
-        author: 'Gary L. Francione',
-        author_id: 'afraid',
-        asset: image2
-    },
-    {
-        title: 'Dinner',
-        description: 'We all love animals. Why do we call some \'pets\' and others \'dinner\'?',
-        author: 'K.D. Lang',
-        author_id: 'i am',
-        asset: image3
-    },
-    {
-        title: 'Appetite or Suffering?',
-        description: 'Could you look an animal in the eyes and say to it, \'My appetite is more important than your suffering\'?',
-        author: 'Moby',
-        author_id: 'depresso expressso',
-        asset: image4
-    },
-    {
-        title: 'The Corpse',
-        description: 'Recognize meat for what it really is: the antibiotic- and pesticide-laden corpse of a tortured animal.',
-        author: 'Ingrid Newkirk',
-        author_id: 'i need coffee',
-        asset: image5
-    },
-    {
-        title: 'Mother\'s Love',
-        description: 'Every time you drink a glass of milk or eat a piece of cheese, you harm a mother. Please go vegan.',
-        author: 'Gary L. Francione',
-        author_id: 'so do i',
-        asset: image6
-    },
-    {
-        title: 'Silent Killer',
-        description: 'Cows\' milk protein may be the single most significant chemical carcinogen to which humans are exposed.',
-        author: 'T. Colin Campbell',
-        author_id: 'when will this end',
-        asset: image7
-    },
-    {
-        title: 'Senseless Suffering',
-        description: 'The question is not, \'Can they reason?\' nor, \'Can they talk?\' but rather, \'Can they suffer?\'',
-        author: 'Jeremy Bentham',
-        author_id: 'i dont know',
-        asset: image8
-    },
-    {
-        title: 'Rabbit Intelligence',
-        description: 'If a rabbit defined intelligence the way man does, then the most intelligent animal would be a rabbit, followed by the animal most willing to obey the commands of a rabbit.',
-        author: 'Robert Brault',
-        author_id: 'almost there',
-        asset: image9
-    },
-    {
-        title: 'Friendly Terms',
-        description: 'Man is the only animal that can remain on friendly terms with the victims he intends to eat until he eats them.',
-        author: 'Samuel Butler',
-        author_id: 'i think',
-        asset: image10
-    },
-    {
-        title: 'Murder of Men',
-        description: 'The time will come when men such as I will look upon the murder of animals as they now look upon the murder of men.',
-        author: 'Leonardo Da Vinci',
-        author_id: 'sheesh',
-        asset: image11
-    },
-    {
-        title: 'Highest Ethics',
-        description: 'on-violence leads to the highest ethics, which is the goal of all evolution. Until we stop harming all other living beings, we are still savages',
-        author: 'Thomas Edison',
-        author_id: 'this keeps going',
-        asset: image1
-    },
-    {
-        title: 'Pleasure, Amusement & Convenience',
-        description: 'We do not need to eat animals, wear animals, or use animals for entertainment purposes, and our only defense of these uses is our pleasure, amusement, and convenience.',
-        author: 'Gary L. Francione',
-        author_id: 'light',
-        asset: image2
-    },
-    {
-        title: 'Dinner',
-        description: 'We all love animals. Why do we call some \'pets\' and others \'dinner\'?',
-        author: 'K.D. Lang',
-        author_id: 'dark',
-        asset: image3
-    },
-    {
-        title: 'Appetite or Suffering?',
-        description: 'Could you look an animal in the eyes and say to it, \'My appetite is more important than your suffering\'?',
-        author: 'Moby',
-        author_id: 'dead',
-        asset: image4
-    },
-    {
-        title: 'The Corpse',
-        description: 'Recognize meat for what it really is: the antibiotic- and pesticide-laden corpse of a tortured animal.',
-        author: 'Ingrid Newkirk',
-        author_id: 'alive',
-        asset: image5
-    },
+
+    // {
+    //     title: 'Mother\'s Love',
+    //     description: 'Every time you drink a glass of milk or eat a piece of cheese, you harm a mother. Please go vegan.',
+    //     author: 'Gary L. Francione',
+    //     author_id: 'funky',
+    //     asset: image6
+    // },
+    // {
+    //     title: 'Silent Killer',
+    //     description: 'Cows\' milk protein may be the single most significant chemical carcinogen to which humans are exposed.',
+    //     author: 'T. Colin Campbell',
+    //     author_id: 'extremely funky',
+    //     asset: image7
+    // },
+    // {
+    //     title: 'Problem solving around the world',
+    //     description: 'In a country you don\'t know and have a problem? No worries! This chart will help you out.',
+    //     author: 'Grady\'s Physics Homework',
+    //     author_id: 'the',
+    //     asset: problems
+    // },
+    // {
+    //     title: 'Senseless Suffering',
+    //     description: 'The question is not, \'Can they reason?\' nor, \'Can they talk?\' but rather, \'Can they suffer?\'',
+    //     author: 'Jeremy Bentham',
+    //     author_id: 'quick',
+    //     asset: image8
+    // },
+    // {
+    //     title: 'Rabbit Intelligence',
+    //     description: 'If a rabbit defined intelligence the way man does, then the most intelligent animal would be a rabbit, followed by the animal most willing to obey the commands of a rabbit.',
+    //     author: 'Robert Brault',
+    //     author_id: 'brown',
+    //     asset: image9
+    // },
+    // {
+    //     title: 'Friendly Terms',
+    //     description: 'Man is the only animal that can remain on friendly terms with the victims he intends to eat until he eats them.',
+    //     author: 'Samuel Butler',
+    //     author_id: 'fox',
+    //     asset: image10
+    // },
+    // {
+    //     title: 'Murder of Men',
+    //     description: 'The time will come when men such as I will look upon the murder of animals as they now look upon the murder of men.',
+    //     author: 'Leonardo Da Vinci',
+    //     author_id: 'are',
+    //     asset: image11
+    // },
+    // {
+    //     title: 'Highest Ethics',
+    //     description: 'on-violence leads to the highest ethics, which is the goal of all evolution. Until we stop harming all other living beings, we are still savages',
+    //     author: 'Thomas Edison',
+    //     author_id: 'you',
+    //     asset: image1
+    // },
+    // {
+    //     title: 'Pleasure, Amusement & Convenience',
+    //     description: 'We do not need to eat animals, wear animals, or use animals for entertainment purposes, and our only defense of these uses is our pleasure, amusement, and convenience.',
+    //     author: 'Gary L. Francione',
+    //     author_id: 'afraid',
+    //     asset: image2
+    // },
+    // {
+    //     title: 'Dinner',
+    //     description: 'We all love animals. Why do we call some \'pets\' and others \'dinner\'?',
+    //     author: 'K.D. Lang',
+    //     author_id: 'i am',
+    //     asset: image3
+    // },
+    // {
+    //     title: 'Appetite or Suffering?',
+    //     description: 'Could you look an animal in the eyes and say to it, \'My appetite is more important than your suffering\'?',
+    //     author: 'Moby',
+    //     author_id: 'depresso expressso',
+    //     asset: image4
+    // },
+    // {
+    //     title: 'The Corpse',
+    //     description: 'Recognize meat for what it really is: the antibiotic- and pesticide-laden corpse of a tortured animal.',
+    //     author: 'Ingrid Newkirk',
+    //     author_id: 'i need coffee',
+    //     asset: image5
+    // },
+    // {
+    //     title: 'Mother\'s Love',
+    //     description: 'Every time you drink a glass of milk or eat a piece of cheese, you harm a mother. Please go vegan.',
+    //     author: 'Gary L. Francione',
+    //     author_id: 'so do i',
+    //     asset: image6
+    // },
+    // {
+    //     title: 'Silent Killer',
+    //     description: 'Cows\' milk protein may be the single most significant chemical carcinogen to which humans are exposed.',
+    //     author: 'T. Colin Campbell',
+    //     author_id: 'when will this end',
+    //     asset: image7
+    // },
+    // {
+    //     title: 'Senseless Suffering',
+    //     description: 'The question is not, \'Can they reason?\' nor, \'Can they talk?\' but rather, \'Can they suffer?\'',
+    //     author: 'Jeremy Bentham',
+    //     author_id: 'i dont know',
+    //     asset: image8
+    // },
+    // {
+    //     title: 'Rabbit Intelligence',
+    //     description: 'If a rabbit defined intelligence the way man does, then the most intelligent animal would be a rabbit, followed by the animal most willing to obey the commands of a rabbit.',
+    //     author: 'Robert Brault',
+    //     author_id: 'almost there',
+    //     asset: image9
+    // },
+    // {
+    //     title: 'Friendly Terms',
+    //     description: 'Man is the only animal that can remain on friendly terms with the victims he intends to eat until he eats them.',
+    //     author: 'Samuel Butler',
+    //     author_id: 'i think',
+    //     asset: image10
+    // },
+    // {
+    //     title: 'Murder of Men',
+    //     description: 'The time will come when men such as I will look upon the murder of animals as they now look upon the murder of men.',
+    //     author: 'Leonardo Da Vinci',
+    //     author_id: 'sheesh',
+    //     asset: image11
+    // },
+    // {
+    //     title: 'Highest Ethics',
+    //     description: 'on-violence leads to the highest ethics, which is the goal of all evolution. Until we stop harming all other living beings, we are still savages',
+    //     author: 'Thomas Edison',
+    //     author_id: 'this keeps going',
+    //     asset: image1
+    // },
+    // {
+    //     title: 'Pleasure, Amusement & Convenience',
+    //     description: 'We do not need to eat animals, wear animals, or use animals for entertainment purposes, and our only defense of these uses is our pleasure, amusement, and convenience.',
+    //     author: 'Gary L. Francione',
+    //     author_id: 'light',
+    //     asset: image2
+    // },
+    // {
+    //     title: 'Dinner',
+    //     description: 'We all love animals. Why do we call some \'pets\' and others \'dinner\'?',
+    //     author: 'K.D. Lang',
+    //     author_id: 'dark',
+    //     asset: image3
+    // },
+    // {
+    //     title: 'Appetite or Suffering?',
+    //     description: 'Could you look an animal in the eyes and say to it, \'My appetite is more important than your suffering\'?',
+    //     author: 'Moby',
+    //     author_id: 'dead',
+    //     asset: image4
+    // },
+    // {
+    //     title: 'The Corpse',
+    //     description: 'Recognize meat for what it really is: the antibiotic- and pesticide-laden corpse of a tortured animal.',
+    //     author: 'Ingrid Newkirk',
+    //     author_id: 'alive',
+    //     asset: image5
+    // },
 ];
